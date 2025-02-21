4e65e54765818cb56ee65678df19cf077bda392b
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-21 12:20:08 +0600

    min number find

---
+++ min.js
@@ -0,0 +1,13 @@
+const value =[23,34,35.56,7676,78666,55655,4545];
+function getMin(numbers){
+    let min= numbers[0];
+   for(const num of numbers){
+      if(num < min){
+        num=min;
+      }
+   }
+   return min;
+}
+
+const minNumber = getMin(value);
+console.log(minNumber)
